101bd2f401c0f796eff6ef4481eceaa3f16bb05c
dancingii <dancingii@yahoo.com>
2025-08-06 10:55:44 -0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import { Buffer } from 'buffer';
+import process from 'process';
+
+window.Buffer = Buffer;
+window.process = process;
 import React, { useState } from "react";
 import { xml2js } from "xml-js";
 
